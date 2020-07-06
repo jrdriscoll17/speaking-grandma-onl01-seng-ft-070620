4382d5071da6d55bc041600dd0c55f3276124a31
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-06 14:51:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,4 +17,6 @@ def speak_to_grandma(phrase)
   if phrase = phrase.upcase
     puts "NO, NOT SINCE 1938!"
   elsif phrase = "I LOVE YOU GRANDMA!"
-    puts 
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts ""
